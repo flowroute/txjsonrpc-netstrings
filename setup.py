747dfc5a57cc76701bd4eebd9412cb1c278921ff
Matthew Williams <matthew@flowroute.com>
2011-08-17 12:58:07 -0500

    version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 setup(
     name='txJsonRpc-Netstrings',
-    version='0.1-alpha1',
+    version='0.2-flowroute',
     description='Lightweight json-rpc library for building Twisted-based TCP (Netstring) clients and servers',
-    author='TelTech Systems',
-    url='http://github.com/teltech/txJsonRpc-Netstrings',
+    author='TelTech Systems, Flowroute',
+    url='http://github.com/mgwilliams/txJsonRpc-Netstrings',
     packages=['txjsonrpc_netstrings']
 )
